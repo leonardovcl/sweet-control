fdba1a7dc080f195ab5f06afc6c9ca52b5631508
Leonardo <leonardovc.lima@gmail.com>
2022-10-06 22:45:12 -0300

    build: Initialize Spring project

---
+++ src/main/java/dev/leonardovcl/sweetcontrol/SweetControlApplication.java
@@ -0,0 +1,13 @@
+package dev.leonardovcl.sweetcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/leonardovcl/sweetcontrol/SweetControlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonardovcl.sweetcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
