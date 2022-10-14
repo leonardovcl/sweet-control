3860cb6c1aa7babb7f97fc96723c612073e552ad
Leonardo <leonardovc.lima@gmail.com>
2022-10-14 01:01:27 -0300

    feat(Recipe): Implement hashCode and equals methods closes #5

--- src/main/java/dev/leonardovcl/sweetcontrol/model/Recipe.java
+++ src/main/java/dev/leonardovcl/sweetcontrol/model/Recipe.java
@@ -1,6 +1,7 @@
 package dev.leonardovcl.sweetcontrol.model;
 
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -72,6 +73,23 @@ public class Recipe {
 		this.recipeIngredients = recipeIngredients;
 	}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recipe other = (Recipe) obj;
+		return Objects.equals(id, other.id);
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder objString = new StringBuilder();
