8c6484b06902a8cd059f2e48538faa565a51085d
Leonardo <leonardovc.lima@gmail.com>
2022-10-22 07:58:37 -0300

    refactor(AmountType): Move AmountType enum from Inventory to Ingredient Model

--- src/main/java/dev/leonardovcl/sweetcontrol/controllers/RecipeIngredientController.java
+++ src/main/java/dev/leonardovcl/sweetcontrol/controllers/RecipeIngredientController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import dev.leonardovcl.sweetcontrol.model.RecipeIngredient;
-import dev.leonardovcl.sweetcontrol.model.repository.IngredientRepository;
 import dev.leonardovcl.sweetcontrol.model.repository.RecipeIngredientRepository;
 
 @Controller
@@ -21,14 +20,9 @@ public class RecipeIngredientController {
 	@Autowired
 	private RecipeIngredientRepository recipeIngredientRepository;
 	
-	@Autowired
-	private IngredientRepository ingredientRepository;
-	
 	@GetMapping("/edit/{idRecipeIngredient}")
 	public String showRecipeIngredientUpdateForm(@PathVariable("idRecipeIngredient") Long idRecipeIngredient, Model model) {
 		model.addAttribute("recipeIngredient", recipeIngredientRepository.findById(idRecipeIngredient).get());
-		model.addAttribute("recipe", recipeIngredientRepository.findById(idRecipeIngredient).get().getRecipe());
-		model.addAttribute("ingredientList", ingredientRepository.findAll());
 		return "recipeIngredientUpdateForm";
 	}
 
@@ -36,8 +30,6 @@ public class RecipeIngredientController {
 	public String updateRecipeIngredient(@PathVariable("idRecipeIngredient") Long idRecipeIngredient, 
 										@Valid RecipeIngredient recipeIngredient) {
 		Long recipeId = recipeIngredientRepository.findById(idRecipeIngredient).get().getRecipe().getId();
-		System.out.println(recipeIngredient.getRecipe());
-		System.out.println(recipeIngredient.getIngredientEntry());
 		recipeIngredientRepository.save(recipeIngredient);
 		
 		return "redirect:/recipes/" + Long.toString(recipeId);
