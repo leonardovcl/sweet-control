12705564507d04d2c61512b6ca3c552fbfbcbc1a
Leonardo <leonardovc.lima@gmail.com>
2022-12-13 16:15:14 -0300

    build(MYSQL DB): Add Native Type Hint for MySQL Dialect

--- src/main/java/dev/leonardovcl/sweetcontrol/SweetControlApplication.java
+++ src/main/java/dev/leonardovcl/sweetcontrol/SweetControlApplication.java
@@ -1,8 +1,14 @@
 package dev.leonardovcl.sweetcontrol;
 
+import org.hibernate.dialect.MySQL8Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeHint;
 
+@NativeHint(types = {
+		@TypeHint(types = MySQL8Dialect.class, typeNames = "org.hibernate.dialect.MySQL8Dialect")
+})
 @SpringBootApplication
 public class SweetControlApplication {
 
